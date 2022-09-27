3824ea11aa6715051df74c96d884c5029e4a5972
abhay-pr <abhay.p@mitsogo.com>
2022-09-27 12:10:32 +0000

    debug

--- maven_build.sh
+++ maven_build.sh
@@ -1,3 +1,3 @@
 pwd
-ls
-docker run --name maven -i --rm -v  $PWD/java-app/build:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine $@
+ls java-app/
+docker run --name maven -i --rm -v  $PWD/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine $@
