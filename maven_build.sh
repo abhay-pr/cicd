e491669484f8252b6a1f59920fbc33bf0e6f3b63
abhay-pr <abhay.p@mitsogo.com>
2022-09-27 11:12:53 +0000

    dir corrected

--- maven_build.sh
+++ maven_build.sh
@@ -1,1 +1,1 @@
-docker run --name maven -i --rm -v $WORKDIR/build:/app -v /root/.m2:/root/.m2 -w /app  maven:3-alpine $@
+docker run --name maven -i --rm -v $WORKDIR/java-app:/app -v /root/.m2:/root/.m2 -w /app  maven:3-alpine $@
